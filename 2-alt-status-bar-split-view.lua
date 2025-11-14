393a2a2fea5faaea59f61920cf17cd9eb33b7386
zenminimalist <zenminimalist@users.noreply.github.com>
2025-11-14 13:40:34 +0100

    Create 2-alt-status-bar-split-view.lua

---
+++ 2-alt-status-bar-split-view.lua
@@ -0,0 +1,192 @@
+--[[
+    KOReader Alt Status Bar Patch
+    Version: 4.0
+    This patch creates an alternative status bar with three sections:
+    - Left: Book Title — Author
+    - Center: Time
+    - Right: Current Page of Total Pages | Progress %
+    
+    Only displayed for reflowable documents (EPUB, etc.), not fixed layout (PDF, CBZ).
+--]]
+
+local Blitbuffer = require("ffi/blitbuffer")
+local TextWidget = require("ui/widget/textwidget")
+local CenterContainer = require("ui/widget/container/centercontainer")
+local VerticalGroup = require("ui/widget/verticalgroup")
+local VerticalSpan = require("ui/widget/verticalspan")
+local HorizontalGroup = require("ui/widget/horizontalgroup")
+local HorizontalSpan = require("ui/widget/horizontalspan")
+local BD = require("ui/bidi")
+local Size = require("ui/size")
+local Geom = require("ui/geometry")
+local Device = require("device")
+local Font = require("ui/font")
+local logger = require("logger")
+local util = require("util")
+local datetime = require("datetime")
+local Screen = Device.screen
+local _ = require("gettext")
+local T = require("ffi/util").template
+local ReaderView = require("apps/reader/modules/readerview")
+local _ReaderView_paintTo_orig = ReaderView.paintTo
+local header_settings = G_reader_settings:readSetting("footer")
+local screen_width = Screen:getWidth()
+
+ReaderView.paintTo = function(self, bb, x, y)
+    _ReaderView_paintTo_orig(self, bb, x, y)
+    if self.render_mode ~= nil then return end -- Show only for epub-likes and never on pdf-likes
+    -- don't change anything above this line
+
+
+
+    -- ===========================!!!!!!!!!!!!!!!=========================== -
+    -- Configure formatting options for header here, if desired
+    local header_font_face = "ffont" -- this is the same font the footer uses
+    -- header_font_face = "source/SourceSerif4-Regular.ttf" -- this is the serif font from Project: Title
+    local header_font_size = header_settings.text_font_size or 14 -- Will use your footer setting if available
+    local header_font_bold = header_settings.text_font_bold or false -- Will use your footer setting if available
+    local header_font_color = Blitbuffer.COLOR_BLACK -- black is the default, but there's 15 other shades to try
+    local header_top_padding = Size.padding.small -- replace small with default or large for more space at the top
+    local header_use_book_margins = false -- Use same margins as book for header
+    local header_margin = Size.padding.large -- Use this instead, if book margins is set to false
+    local left_max_width_pct = 40 -- this % is how much space the left section can use before "truncating..."
+    local center_max_width_pct = 15 -- this % is how much space the center section can use before "truncating..."
+    local right_max_width_pct = 40 -- this % is how much space the right section can use before "truncating..."
+    local separator = {
+        bar     = "|",
+        bullet  = "•",
+        dot     = "·",
+        em_dash = "—",
+        en_dash = "-",
+    }
+    -- ===========================!!!!!!!!!!!!!!!=========================== -
+
+
+
+    -- You probably don't need to change anything in the section below this line
+    
+    -- Title and Author(s):
+    local book_title = ""
+    local book_author = ""
+    if self.ui.doc_props then
+        book_title = self.ui.doc_props.display_title or ""
+        book_author = self.ui.doc_props.authors or ""
+        if book_author:find("\n") then -- Show first author if multiple authors
+            book_author =  T(_("%1 et al."), util.splitToArray(book_author, "\n")[1] .. ",")
+        end
+    end
+    -- Page count and percentage
+    local pageno = self.state.page or 1 -- Current page
+    local pages = self.ui.doc_settings.data.doc_pages or 1
+    local page_progress = ("%d of %d"):format(pageno, pages)
+    local pages_left_book  = pages - pageno
+    local percentage = (pageno / pages) * 100 -- Format like %.1f in header_string below
+    -- Chapter Info
+    local book_chapter = ""
+    local pages_chapter = 0
+    local pages_left = 0
+    local pages_done = 0
+    if self.ui.toc then
+        book_chapter = self.ui.toc:getTocTitleByPage(pageno) or "" -- Chapter name
+        pages_chapter = self.ui.toc:getChapterPageCount(pageno) or pages
+        pages_left = self.ui.toc:getChapterPagesLeft(pageno) or self.ui.document:getTotalPagesLeft(pageno)
+        pages_done = self.ui.toc:getChapterPagesDone(pageno) or 0
+    end
+    pages_done = pages_done + 1 -- This +1 is to include the page you're looking at
+    local chapter_progress = pages_done .. " ⁄⁄ " .. pages_chapter
+    -- Clock:
+    local time = datetime.secondsToHour(os.time(), G_reader_settings:isTrue("twelve_hour_clock")) or ""
+    -- Battery:
+    local battery = ""
+    if Device:hasBattery() then
+        local power_dev = Device:getPowerDevice()
+        local batt_lvl = power_dev:getCapacity() or 0
+        local is_charging = power_dev:isCharging() or false
+        local batt_prefix = power_dev:getBatterySymbol(power_dev:isCharged(), is_charging, batt_lvl) or ""
+        battery = batt_prefix .. batt_lvl .. "%"
+    end
+    -- You probably don't need to change anything in the section above this line
+
+
+
+    -- ===========================!!!!!!!!!!!!!!!=========================== -
+    -- What you put here will show in the header:
+    local left_header = string.format("%s %s %s", book_title, separator.em_dash, book_author)
+    local center_header = string.format("%s", time)
+    local right_header = string.format("%s %s %.1f%%", page_progress, separator.bar, percentage)
+    -- Look up "string.format" in Lua if you need help.
+    -- ===========================!!!!!!!!!!!!!!!=========================== -
+
+
+    -- don't change anything below this line
+    local margins = 0
+    local left_margin = header_margin
+    local right_margin = header_margin
+    if header_use_book_margins then -- Set width % based on R + L margins
+        left_margin = self.document:getPageMargins().left or header_margin
+        right_margin = self.document:getPageMargins().right or header_margin
+    end
+    margins = left_margin + right_margin
+    local avail_width = screen_width - margins -- deduct margins from width
+    local function getFittedText(text, max_width_pct)
+        if text == nil or text == "" then
+            return ""
+        end
+        local text_widget = TextWidget:new{
+            text = text:gsub(" ", "\u{00A0}"), -- no-break-space
+            max_width = avail_width * max_width_pct * (1/100),
+            face = Font:getFace(header_font_face, header_font_size),
+            bold = header_font_bold,
+            padding = 0,
+        }
+        local fitted_text, add_ellipsis = text_widget:getFittedText()
+        text_widget:free()
+        if add_ellipsis then
+            fitted_text = fitted_text .. "…"
+        end
+        return BD.auto(fitted_text)
+    end
+    left_header = getFittedText(left_header, left_max_width_pct)
+    center_header = getFittedText(center_header, center_max_width_pct)
+    right_header = getFittedText(right_header, right_max_width_pct)
+    local left_header_text = TextWidget:new {
+        text = left_header,
+        face = Font:getFace(header_font_face, header_font_size),
+        bold = header_font_bold,
+        fgcolor = header_font_color,
+        padding = 0,
+    }
+    local center_header_text = TextWidget:new {
+        text = center_header,
+        face = Font:getFace(header_font_face, header_font_size),
+        bold = header_font_bold,
+        fgcolor = header_font_color,
+        padding = 0,
+    }
+    local right_header_text = TextWidget:new {
+        text = right_header,
+        face = Font:getFace(header_font_face, header_font_size),
+        bold = header_font_bold,
+        fgcolor = header_font_color,
+        padding = 0,
+    }
+    -- Calculate dynamic spacing to center the middle element
+    local total_text_width = left_header_text:getSize().w + center_header_text:getSize().w + right_header_text:getSize().w
+    local remaining_width = avail_width - total_text_width
+    local left_space = (avail_width - center_header_text:getSize().w) / 2 - left_header_text:getSize().w
+    local right_space = remaining_width - left_space
+    local header = CenterContainer:new {
+        dimen = Geom:new{ w = screen_width, h = math.max(left_header_text:getSize().h, center_header_text:getSize().h, right_header_text:getSize().h) + header_top_padding },
+        VerticalGroup:new {
+            VerticalSpan:new { width = header_top_padding },
+            HorizontalGroup:new {
+                left_header_text,
+                HorizontalSpan:new { width = left_space },
+                center_header_text,
+                HorizontalSpan:new { width = right_space },
+                right_header_text,
+            }
+        },
+    }
+    header:paintTo(bb, x, y)
+end
